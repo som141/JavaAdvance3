15ae40150496007ed94225ce1fbeee8157d9bc4d
som141 <163431679+som141@users.noreply.github.com>
2025-03-25 12:41:44 +0900

    lamda study1

--- src/lamda/EX0ReMain.java
+++ src/lamda/EX0ReMain.java
@@ -1,18 +1,15 @@
 package lamda;
-
+//변하지 않는 부분과 변하는 부분을 분리해야한다.
 public class EX0ReMain {
-    public static void helloJava() {
+//    value parameterization(값을 외부에서 받아 처리)
+    public static void hello(String stirng) {
         System.out.println("프로그램 시작");
-        System.out.println("Hello Java");
-        System.out.println("프로그램 종료");
-    }
-    public static void helloSpring() {
-        System.out.println("프로그램 시작");
-        System.out.println("Hello Spring");
+        System.out.println(stirng);     
         System.out.println("프로그램 종료");
     }
     public static void main(String[] args) {
-        helloJava();
-        helloSpring();
+
+        hello("hello java");
+        hello("hello spring");
     }
 }
