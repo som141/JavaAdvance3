016371db9dba50d9923235be1a9d07a2cc7aea70
som141 <163431679+som141@users.noreply.github.com>
2025-04-29 13:02:24 +0900

    s

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange(, String.class);
     }
 }
