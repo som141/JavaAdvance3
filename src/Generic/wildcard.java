a6a0c887b1090fe5d7fe81a3d4b405217456a166
som141 <163431679+som141@users.noreply.github.com>
2025-05-14 13:16:01 +0900

    s
    s

--- src/Generic/wildcard.java
+++ src/Generic/wildcard.java
@@ -1,10 +1,16 @@
 package Generic;
 
-public class wildcard {
-    static <T> void l((T extend Number) a){
+import java.util.ArrayList;
+import java.util.List;
 
+public class wildcard {
+    static <T extends Integer> void l(T t){
+//        System.out.println(t*t); 인티저 끼리의 곱은 허용이 안됨.
+        int t1=t;
+        System.out.println(t);
     }
     public static void main(String[] args) {
-
+        l(1);
+        List<?> list = new ArrayList<>();
     }
 }
